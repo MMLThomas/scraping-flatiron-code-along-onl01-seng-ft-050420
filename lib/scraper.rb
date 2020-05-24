0889cfc58e48bb4263d40d2629a8354527e0c7c9
michael thomas <michael.thomas2014@gmail.com>
2020-05-24 17:58:21 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,6 +23,10 @@ class Scraper
    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
 
   end
+  
+  def get_courses
+    slef.get_page.css(".post")
+  end
 
   
  
